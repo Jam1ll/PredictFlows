6f353b34083f746481b9331b390f3a5b678ec5fd
Jamil Guzmán <20240100@itla.edu.do>
2025-10-09 18:19:37 -0400

    commit rapido

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CryptoView, CryptoHistoryView, crypto_stream, PrediccionCryptoView
+from .views import CryptoView, CryptoHistoryView, crypto_stream, PrediccionCryptoView, StockHistoryView, StockView, StockStream
 
 urlpatterns = [
     path('cryptocoin/', CryptoView.as_view(), name='crypto-all'),
@@ -7,7 +7,7 @@ urlpatterns = [
     path('cryptocoin/stream/', crypto_stream, name='crypto-stream'),
     path('cryptocoin/stream/<int:num_coins>', crypto_stream, name='crypto-stream-filtered'),
     path('prediccion/', PrediccionCryptoView.as_view(), name='prediccion-crypto'),
-    # path('stocks/', StockView.as_view(), name='stocks-all'),
-    # path('stocks/history/<int:days>/', StockHistoryView.as_view(), name='stocks-history'),
-    # path('stocks/stream/', StockStream.as_view(), name='stocks-stream'),
+    path('stocks/', StockView.as_view(), name='stocks-all'),
+    path('stocks/history/<int:days>/', StockHistoryView.as_view(), name='stocks-history'),
+    path('stocks/stream/', StockStream.as_view(), name='stocks-stream'),
 ]
